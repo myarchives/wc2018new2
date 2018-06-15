7279291b354fc3e45a7a6700211683a14d2350b6
xemtivi <xemtivionlinevn@gmail.com>
2018-06-15 15:43:46 +0700

    add cannel

---
+++ tvcheckbymev1.php
@@ -0,0 +1,53 @@
+{
+   "check_update":"false",
+   "title_update":"T\u1ea3i app xem b\u00f3ng \u0111\u00e1 b\u00ecnh lu\u1eadn TV",
+   "link_update":"https://play.google.com/store/apps/details?id=com.sporttv.xembongda&hl=vi",
+   "streaming":[
+      {
+         "status":"yes"
+      }
+   ],
+   "channels":[
+      {
+         "Name":"Kênh ca nhạc",
+         "url":"https://lh3.googleusercontent.com/InRT5mbV9khyeIaSMSWNQNucuDIVVBbi5dPkmIDSupLIpKgaPUbjF0mrhgCBeofpaFqC-i6XSmMF2MlOlyv5ExQvvbNMay24jDNtVmVXNIZ97CP8Hv84KZt-8inn37F8POoQ5c_Jzw=m18",
+         "url1":"",
+         "url2":"",
+         "url3":"",
+         "url4":"",
+         "url5":"",
+         "image":"https://i.ytimg.com/vi/6t16E4YJi_o/maxresdefault.jpg"
+      },
+      {
+         "Name":"Giải trí",
+         "url":"https://lh3.googleusercontent.com/Aloy1WT4nVribw0qEtMaokDS6Lha0CsjdLt4bvilMq2bH5qtzgzMALFBiOZBDUpSmvRpPBPJGg9Cie9uc1IjmyCQ5_mbA8c8zbhK-DPChO7MWGLwaQWmLQNHXr9sxLncabFw1EFSZQ=m18",
+         "url1":"",
+         "url2":"",
+         "url3":"",
+         "url4":"",
+         "url5":"",
+         "image":"https://i.ytimg.com/vi/NdVDyYSGab4/maxresdefault.jpg"
+      },
+      {
+         "Name":"Game",
+         "url":"https://lh3.googleusercontent.com/Gi6iAT0aNm6Hs9NRkx82sVlHpe25H1zLSU1QU9sdBG8MyS46aqASXUQy9vbBD82dboID1odO5hU1g5TjreMfoZCvMnRXYtWqV7wL9OiJ5UN-EiqgfJ7stubD9Bxfs8vAJ0zjkbE0IQ=m18",
+         "url1":"",
+         "url2":"",
+         "url3":"",
+         "url4":"",
+         "url5":"",
+         "image":"https://i.ytimg.com/vi/UKlwcAIiSxo/maxresdefault.jpg"
+      }
+	  ,
+      {
+         "Name":"Thể thao",
+         "url":"https://lh3.googleusercontent.com/Gi6iAT0aNm6Hs9NRkx82sVlHpe25H1zLSU1QU9sdBG8MyS46aqASXUQy9vbBD82dboID1odO5hU1g5TjreMfoZCvMnRXYtWqV7wL9OiJ5UN-EiqgfJ7stubD9Bxfs8vAJ0zjkbE0IQ=m18",
+         "url1":"",
+         "url2":"",
+         "url3":"",
+         "url4":"",
+         "url5":"",
+         "image":"https://znews-photo-td.zadn.vn/w1024/Uploaded/abhuuah/2014_06_12/34444.jpg"
+      }
+   ]
+}
